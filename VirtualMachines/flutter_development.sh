bcdf85b75fb264164b50aed34b1b1b745b72d6e8
JustScott <business@justscott.me>
2023-10-13 16:36:27 -0500

    Added a new command for the user to try in the README to attempt to fix the key issues whe using pacman before installation

--- VirtualMachines/flutter_development.sh
+++ VirtualMachines/flutter_development.sh
@@ -16,9 +16,36 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-
 SCRIPT_DIR=../NonUserRunnable/backup_scripts/flutter_development
 
 mkdir -p ~/.scripts/general
 sudo ln -sf $PWD/$SCRIPT_DIR/pack /usr/local/bin/pack
 sudo ln -sf $PWD/$SCRIPT_DIR/unpack /usr/local/bin/unpack
+
+
+yay -Sy \
+    dart android-tools flutter \
+    android-studio android-sdk android-sdk-cmdline-tools-latest adb \
+    clang cmake ninja base-devel --noconfirm
+
+
+flutter create test_flutter_app 
+
+echo -e "\n------\n1. Complete the android studio prompts to continue the installation"
+echo "2. Close the Android studio project page and a new one will reopen with a Flutter project that was created for you, click 'Trust Project'"
+echo "3. Go to file -> settings -> Android SDK -> SDK Tools."
+echo "4. Download 'Android SDK Command-line Tools (latest)' by checking the box and clicking apply."
+echo -e "Exit android studio to allow the script to continue running\n------\n"
+
+android-studio > /dev/null 2>&1
+
+android-studio test_flutter_app > /dev/null 2>&1  
+
+# Suppresses an error
+git config --global --add safe.directory /opt/flutter
+
+flutter doctor --android-licenses
+
+flutter config --android-sdk ~/Android/Sdk
+
+rm -rf test_flutter_app
