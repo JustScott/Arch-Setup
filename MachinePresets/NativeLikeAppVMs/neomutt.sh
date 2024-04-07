0fc9259ddde2890773d944d938ac3a2894053dac
JustScott <development@justscott.me>
2024-04-07 10:39:10 -0500

    Open lynx & open neomutt in their VMs when a new terminal session is opened.

--- MachinePresets/NativeLikeAppVMs/neomutt.sh
+++ MachinePresets/NativeLikeAppVMs/neomutt.sh
@@ -47,6 +47,10 @@ pacman -Q ${packages[@]} &>/dev/null || {
     sudo make install
 }
 
+grep "neomutt" $HOME/.bash_profile &>/dev/null || {
+    echo -e "\nneomutt" $HOME/.bash_profile
+}
+
 cd $STARTING_PWD
 
 cd ..
