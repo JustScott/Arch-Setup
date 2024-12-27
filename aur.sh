c16e417c6588ba2193fa4db53dd7cf4a2baa85ac
JustScott <development@justscott.me>
2024-12-27 16:24:03 -0600

    Add aur.sh script for installing the yay package manager

---
+++ aur.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# aur.sh - part of the Arch-Setup project
+# Copyright (C) 2023, Scott Wyman, development@scottwyman.me
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+OLD_PWD=$PWD
+
+if ! { which yay || type yay; } &>/dev/null
+then
+    ACTION="Clone, compile, and install yay from the AUR (this may take a while)"
+    echo -n "...$ACTION..."
+    cd # pwd -> $HOME
+    if git clone https://aur.archlinux.org/yay.git >/dev/null 2>>/tmp/archsetuperrors.log
+    then
+        {
+            cd yay >/dev/null 2>>/tmp/archsetuperrors.log
+            makepkg -si PKGBUILD --noconfirm >/dev/null 2>>/tmp/archsetuperrors.log
+            cd $OLD_PWD
+        } >/dev/null 2>>/tmp/archsetuperrors.log \
+            && echo "[SUCCESS]" \
+            || { echo "[FAIL] wrote error log to /tmp/archsetuperrors.log"; exit; }
+    else
+        echo "[FAIL] wrote error log to /tmp/archsetuperrors.log"
+        exit 1
+    fi
+fi
+
+cd $OLD_PWD
