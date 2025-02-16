2228cfa3ecc83b2ae641eda05e17b56c9d4dbabe
JustScott <development@justscott.me>
2025-02-16 16:49:31 -0600

    Add emoji support to river

--- river.sh
+++ river.sh
@@ -22,7 +22,7 @@ packages=(
     wayland-protocols river foot wl-clipboard bemenu-wayland \
     swaybg wlr-randr \
     swayidle waylock \
-    brightnessctl
+    brightnessctl noto-fonts-emoji
 )
 
 if ! pacman -Q pulseaudio zig &>/dev/null
