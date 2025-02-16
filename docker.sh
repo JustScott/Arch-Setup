90fbccb057ad4727b00b8273bf9e138fb86ff54b
JustScott <development@justscott.me>
2025-02-16 13:50:49 -0600

    Avoid duplication errors when running docker.sh multiple times

--- docker.sh
+++ docker.sh
@@ -16,11 +16,16 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-ACTION="Install docker and docker-compose"
-echo -n "...$ACTION..."
-sudo pacman -Sy docker docker-compose --noconfirm >/dev/null 2>>/tmp/archsetuperrors.log \
-    && echo "[SUCCESS]" \
-    || { echo "[FAIL] wrote error log to /tmp/archsetuperrors.log"; exit; }
+
+packages=(docker docker-compose)
+
+if ! pacman -Q ${packages[@]} &>/dev/null; then
+    ACTION="Install docker and docker-compose"
+    echo -n "...$ACTION..."
+    sudo pacman -Sy  --noconfirm >/dev/null 2>>/tmp/archsetuperrors.log \
+        && echo "[SUCCESS]" \
+        || { echo "[FAIL] wrote error log to /tmp/archsetuperrors.log"; exit; }
+fi
 
 ACTION="Configure Docker"
 if sudo systemctl enable --now docker >/dev/null 2>>/tmp/archsetuperrors.log
@@ -33,8 +38,10 @@ fi
 if [[ -f $HOME/.bashrc ]]; then
     ACTION="Set Docker defaults in $HOME/.bashrc"
     {
-        echo -e "\nexport DOCKER_BUILDKIT=1" >> $HOME/.bashrc
-        echo -e "export COMPOSE_DOCKER_CLI_BUILD=1\n" >> $HOME/.bashrc
+        cat $HOME/.bashrc | grep "export DOCKER_BUILDKIT=1" >/dev/null || \
+            echo -e "\nexport DOCKER_BUILDKIT=1" >> $HOME/.bashrc
+        cat $HOME/.bashrc | grep "export COMPOSE_DOCKER_CLI_BUILD=1" >/dev/null || \
+            echo -e "export COMPOSE_DOCKER_CLI_BUILD=1\n" >> $HOME/.bashrc
     } >/dev/null 2>>/tmp/archconfigurationerrors.log \
         && echo "[SUCCESS] $ACTION" \
         || echo "[FAIL] $ACTION... wrote error log to /tmp/archsetuperrors.log"
