e53f4ccb2d3fa43bcf91b64c0b6f036c823e3398
JustScott <development@justscott.me>
2025-09-17 11:01:27 -0500

    Add installation script for 3D printing

---
+++ 3d-printing.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# 3d-printing.sh - part of the Arch-Setup project
+# Copyright (C) 2023-2025, JustScott, development@justscott.me
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+if [[ $(basename $PWD) != "Arch-Setup" ]]
+then
+    printf "\e[31m%s\e[0m\n" \
+        "[Error] Please run script from the Arch-Setup base directory"
+    exit 1
+fi
+
+source ./shared_lib
+
+sudo -v
+
+packages=(freecad prusa-slicer)
+
+if ! pacman -Q ${packages[@]} &>/dev/null; then
+    sudo -v
+    sudo pacman -Sy ${packages[@]} --noconfirm >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+    task_output $! "$STDERR_LOG_PATH" \
+        "Install CAD and Slicer tools"
+    [[ $? -ne 0 ]] && exit 1
+fi
