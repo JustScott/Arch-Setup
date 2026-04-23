f26d499d0368ce5601d4ae382b2cf321786490f9
JustScott <development@justscott.me>
2026-04-23 11:27:37 -0500

    Greatly improve and reduce the logic in install_configurations.sh (arch add-configs)

--- install_configurations.sh
+++ install_configurations.sh
@@ -32,187 +32,132 @@ fi
 
 CONFIGS_DIRECTORY="$(pwd)/Configurations"
 
+configure_bashrc_extension()
 {
-    extension_path="$CONFIGS_DIRECTORY/bashrc_extension"
-    if ! grep "source $extension_path" $HOME/.bashrc &>/dev/null
+    local extension_path="${CONFIGS_DIRECTORY}/bashrc_extension"
+    if ! grep "^source $extension_path$" "${HOME}/.bashrc" &>/dev/null
     then
-        echo -e "\nsource $extension_path" \
-            >> $HOME/.bashrc 2>>"$STDERR_LOG_PATH" &
+        echo "source $extension_path" >> "${HOME}/.bashrc" 2>>"$STDERR_LOG_PATH" &
         task_output $! "$STDERR_LOG_PATH" \
-            "Source bashrc_extension in $HOME/.bashrc"
+            "Source bashrc_extension in ${HOME}/.bashrc"
     fi
 } 
 
-if { which nvim || type nvim; } &>/dev/null
-then
-    if ! [[ -L "$HOME/.vimrc" ]]
+configure_vim()
+{
+    if command -v vim &>/dev/null
     then
-        ln -sf $CONFIGS_DIRECTORY/init.vim $HOME/.vimrc \
-            >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-        task_output $! "$STDERR_LOG_PATH" \
-            "Create soft link for '\$HOME/.vimrc'"
-    fi
-
-    if ! [[ -d "$HOME/.config/nvim" && -L "$HOME/.config/nvim/init.vim" ]]
-    then
-        {
-            mkdir -p $HOME/.config/nvim
-            ln -sf $CONFIGS_DIRECTORY/init.vim $HOME/.config/nvim
-        } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-        task_output $! "$STDERR_LOG_PATH" \
-            "Create soft link for '\$HOME/.config/nvim'"
-    fi
-
-    # Install Vim-Plug for adding pluggins to vim and neovim
-    if ! [ -f "$HOME/.local/share/nvim/site/autoload/plug.vim" ]
-    then
-        mkdir -p $HOME/.local/share/nvim/site/autoload &>/dev/null
-        {
-            curl -Lo $HOME/.local/share/nvim/site/autoload/plug.vim \
-                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-            nvim -c "PlugInstall | qall" --headless
-        } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-        task_output $! "$STDERR_LOG_PATH" "Download & Install vim-plug"
-    fi
-fi
-
-if { which git || type git; } &>/dev/null
-then
-    git_config="$(git config --list)"
-    if ! echo "$git_config" | grep "user.email=development@justscott.me" \
-        &>/dev/null
-    then
-        git config --global user.email development@justscott.me \
-            >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-        task_output $! "$STDERR_LOG_PATH" \
-            "Configure global git user.email as development@justscott.me"
-    fi
-
-    if ! echo "$git_config" | grep "user.name=JustScott" \
-        &>/dev/null
-    then
-        git config --global user.name JustScott \
-            >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-        task_output $! "$STDERR_LOG_PATH" \
-            "Configure global git user.name as JustScott"
-    fi
-
-    if { which bat || type bat; } &>/dev/null
-    then
-        if ! echo "$git_config" | grep \
-            "core.pager=bat --paging=always --style=changes" \
-            &>/dev/null
+        if ! cat "${HOME}/.vimrc" &>/dev/null
         then
-            git config --global core.pager \
-                "bat --paging=always --style=changes" \
-                >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+            ln -sf "${CONFIGS_DIRECTORY}/nvim/init.vim" \
+                 "${HOME}/.vimrc" \
+                 >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
             task_output $! "$STDERR_LOG_PATH" \
-                "Configure global git core.pager as bat"
+                "Create soft link to '\$HOME/.vimrc'"
         fi
     fi
-fi
+}
 
-if { which lf || type lf; } &>/dev/null
-then
-    if ! [[ \
-        -d "$HOME/.config/lf" \
-        && -L "$HOME/.config/lf/lfrc" \
-        && -L "$HOME/.config/lf/previewer.sh" \
-    ]]
+configure_vim_plug()
+{
+    if command -v nvim &>/dev/null
     then
-        {
-            mkdir -p $HOME/.config/lf
-            ln -sf $CONFIGS_DIRECTORY/lf/{lfrc,previewer.sh} $HOME/.config/lf/
-        } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-        task_output $! "$STDERR_LOG_PATH" \
-            "Create soft link to '\$HOME/.config/lf/{lfrc,previewer.sh}'"
+        # Install Vim-Plug for adding pluggins to vim and neovim
+        if ! [[ -f "${HOME}/.local/share/nvim/site/autoload/plug.vim" ]]
+        then
+            mkdir -p "${HOME}/.local/share/nvim/site/autoload" &>/dev/null
+
+            curl -Lo "${HOME}/.local/share/nvim/site/autoload/plug.vim" \
+                https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
+                >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+            task_output $! "$STDERR_LOG_PATH" "Download & Install vim-plug"
+
+            nvim -c "PlugInstall | qall" --headless \
+                >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+            task_output $! "$STDERR_LOG_PATH" "Install plugins with vim-plug"
+        fi
     fi
-fi
+}
 
-if { which calcurse || type calcurse; } &>/dev/null
-then
-    if ! [[ -d "$HOME/.config/calcurse" && -L "$HOME/.config/calcurse/conf" ]]
+configure_git()
+{
+    if command -v git &>/dev/null
     then
-        {
-            mkdir -p $HOME/.config/calcurse
-            ln -sf $CONFIGS_DIRECTORY/calcurse/conf $HOME/.config/calcurse/
-        } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-        task_output $! "$STDERR_LOG_PATH" \
-            "Create soft link to '\$HOME/.config/calcurse/conf'"
-    fi
-fi
+        local git_config="$(git config --list)"
 
-if { which bat || type bat; } &>/dev/null
-then
-    if ! [[ -d "$HOME/.config/bat" && -L "$HOME/.config/bat/config" ]]
-    then
-        {
-            mkdir -p $HOME/.config/bat
-            ln -sf $CONFIGS_DIRECTORY/bat/config $HOME/.config/bat/config
-        } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-        task_output $! "$STDERR_LOG_PATH" \
-            "Create soft link to '\$HOME/.config/bat/config'"
-    fi
-fi
-
-if { which newsboat || type newsboat; } &>/dev/null
-then
-    if ! [[ \
-        -d "$HOME/.config/newsboat" \
-        && -L "$HOME/.config/newsboat/config" \
-        && -L "$HOME/.config/newsboat/urls" \
-    ]]
-    then
-        {
-            mkdir -p $HOME/.config/newsboat
-            ln -sf $CONFIGS_DIRECTORY/newsboat/{config,urls} \
-                $HOME/.config/newsboat/
-        } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+        if ! echo "$git_config" | grep "user.email=development@justscott.me" \
+            &>/dev/null
+        then
+            git config --global user.email development@justscott.me \
+                >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
             task_output $! "$STDERR_LOG_PATH" \
-                "Create soft link to '\$HOME/.config/newsboat/{config,urls}'"
-    fi
-fi
-
-if { which mpv || type mpv; } &>/dev/null
-then
-    if ! [[ -d "$HOME/.config/mpv" && -L "$HOME/.config/mpv/mpv.conf" ]]
-    then
-        {
-            mkdir -p $HOME/.config/mpv
-            ln -sf $CONFIGS_DIRECTORY/mpv/mpv.conf $HOME/.config/mpv/
-        } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+                "Configure global git user.email as development@justscott.me"
+        fi
+        if ! echo "$git_config" | grep "user.name=JustScott" \
+            &>/dev/null
+        then
+            git config --global user.name JustScott \
+                >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
             task_output $! "$STDERR_LOG_PATH" \
-                "Create soft link to '\$HOME/.config/mpv/mpv.conf'"
-    fi
-fi
+                "Configure global git user.name as JustScott"
+        fi
 
-if { which ytfzf || type ytfzf; } &>/dev/null
-then
-    if ! [[ -d "$HOME/.config/ytfzf" && -L "$HOME/.config/ytfzf/conf.sh" ]]
-    then
-        {
-            mkdir -p $HOME/.config/ytfzf
-            ln -sf $CONFIGS_DIRECTORY/ytfzf/conf.sh $HOME/.config/ytfzf/
-        } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
-            task_output $! "$STDERR_LOG_PATH" \
-                "Create soft link to '\$HOME/.config/ytfzf/conf.sh'"
+        if command -v bat &>/dev/null
+        then
+            if ! echo "$git_config" | grep \
+                "core.pager=bat --paging=always --style=changes" \
+                &>/dev/null
+            then
+                git config --global core.pager \
+                    "bat --paging=always --style=changes" \
+                    >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+                task_output $! "$STDERR_LOG_PATH" \
+                    "Configure global git core.pager as bat"
+            fi
+        fi
     fi
-fi
+}
 
-if { which qutebrowser || type qutebrowser; } &>/dev/null
-then
-    if ! [[ \
-        -d "$HOME/.config/qutebrowser" \
-        && -L "$HOME/.config/qutebrowser/blocked-hosts" \
-        && -L "$HOME/.config/qutebrowser/quickmarks" \
-    ]]
+configure_tool()
+{
+    local tool="$1"
+    shift
+    local file_names=("$@")
+
+    if command -v $tool &>/dev/null
     then
-        {
-            mkdir -p $HOME/.config/qutebrowser
-            ln -sf $CONFIGS_DIRECTORY/qutebrowser/{blocked-hosts,quickmarks} \
-                $HOME/.config/qutebrowser/
-        } >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+        if ! [[  -d "${HOME}/.config/${tool}" ]]
+        then
+            mkdir -p "${HOME}/.config/${tool}" \
+                >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
             task_output $! "$STDERR_LOG_PATH" \
-                "Create soft link to '\$HOME/.config/qutebrowser/{blocked-hosts,quickmarks}'"
-    fi
-fi
+                "Create the '\$HOME/.config/${tool}' directory"
+        fi
+
+        for file_name in ${file_names[@]}
+        do
+            if ! cat "${HOME}/.config/${tool}/${file_name}" &>/dev/null
+            then
+                ln -sf "${CONFIGS_DIRECTORY}/${tool}/${file_name}" \
+                     "${HOME}/.config/${tool}/" \
+                     >>"$STDOUT_LOG_PATH" 2>>"$STDERR_LOG_PATH" &
+                task_output $! "$STDERR_LOG_PATH" \
+                    "Create soft link to '\$HOME/.config/${tool}/${file_name}'"
+            fi
+        done
+    fi 
+}
+
+configure_tool nvim init.vim
+configure_tool bat config
+configure_tool mpv mpv.conf
+configure_tool ytfzf conf.sh
+configure_tool calcurse conf
+configure_tool lf lfrc previewer.sh
+configure_tool newsboat config urls
+configure_tool qutebrowser blocked-hosts quickmarks
+
+configure_bashrc_extension
+configure_vim
+configure_vim_plug
+configure_git
